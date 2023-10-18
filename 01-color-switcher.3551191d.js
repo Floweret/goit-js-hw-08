7cd8b5b5f6d7b7b3bf859fe9c90b55be76a2122f
Floweret <rush.anastasia@gmail.com>
2023-10-18 16:38:56 +0000

    Deploying to gh-pages from @ Floweret/goit-js-hw-08@5fe53e5d3694feb33e52aedb02f19f12dc44486c 🚀

---
+++ 01-color-switcher.3551191d.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),n=document.querySelector("body");e.addEventListener("click",(function(d){e.disabled=!0,a.disabled=!1,t=setInterval((function(){n.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3)})),a.addEventListener("click",(function(n){a.disabled=!0,e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.3551191d.js.map
